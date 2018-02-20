23e598baba9538783945bbcad74e8debd040de74
stumason <stu@stuartmason.co.uk>
2018-02-20 13:08:54 +0000

    codeship debugged

--- deployment/codeship.sh
+++ deployment/codeship.sh
@@ -3,9 +3,6 @@ phpenv local 7.1
 mkdir -p ./bootstrap/cache
 cp .env.codeship .env
 composer install --no-interaction --no-scripts
-mysql -e 'create database testing;'
-mysql -e 'CREATE USER testing@localhost IDENTIFIED BY secret;'
-mysql -e 'GRANT ALL PRIVILEGES ON * . * TO test@localhost;'
 php artisan migrate:fresh --seed
 ./vendor/laravel/dusk/bin/chromedriver-linux &
 php artisan serve > /dev/null 2>&1 &
