ae3354dce15268ee22b738200f05d776cfeb5644
stumason <stu@stuartmason.co.uk>
2018-02-20 12:47:13 +0000

    .env race condition

--- deployment/codeship.sh
+++ deployment/codeship.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 phpenv local 7.1
 mkdir -p ./bootstrap/cache
-composer install --no-interaction
+cp .env.codeship .env
+composer install --no-interaction --no-scripts
 mysql -e 'create database testing;'
 mysql -e 'CREATE USER testing@localhost IDENTIFIED BY secret;'
 mysql -e 'GRANT ALL PRIVILEGES ON * . * TO test@localhost;'
 php artisan migrate:fresh --seed
 ./vendor/laravel/dusk/bin/chromedriver-linux &
-cp .env.codeship .env
 php artisan serve > /dev/null 2>&1 &
 sleep 3
