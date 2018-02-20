a773a892a0880e7191a6fb11395473df9ac48d07
stumason <stu@stuartmason.co.uk>
2018-02-20 12:21:02 +0000

    socialite start

--- deployment/codeship.sh
+++ deployment/codeship.sh
@@ -2,6 +2,7 @@
 phpenv local 7.1
 mkdir -p ./bootstrap/cache
 composer install --no-interaction
+curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/mysql-5.7.sh | bash -s
 mysql -e 'create database forge;'
 mysql -e 'CREATE USER forge@localhost IDENTIFIED BY secret;'
 mysql -e 'GRANT ALL PRIVILEGES ON * . * TO forge@localhost;'
