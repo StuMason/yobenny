9440f7e7012e221911ddc8b2cf7aaf54285eaf62
stumason <stu@stuartmason.co.uk>
2018-03-21 10:07:50 +0000

    mainline deployment!

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,5 @@ dt=$(date '+%d/%m/%Y %H:%M:%S');
 git remote add fortrabbit yobenny@deploy.eu2.frbit.com:yobenny.git
 git checkout -b yobenny
 git add .
-git commit -m "deployment - $dt"
+git commit -m "mainline deployment $dt"
 git push fortrabbit yobenny
